6a931b17a08404e92037200bebc35f7024d2f6c5
s14k51 <s14k51@gmail.com>
2020-04-28 15:47:40 -0500

    feat: use custom config
    Closes: #7

--- __tests__/no-wip.spec.js
+++ __tests__/no-wip.spec.js
@@ -4,7 +4,22 @@ describe('no-wip config', () => {
   it('expected', () => {
     expect(config).toStrictEqual({
       defaultIgnores: false,
-      extends: ['@commitlint/config-conventional'],
+      extends: ['./index.js'],
+      rules: {
+        'type-enum': [2, 'always', [
+          'feat',
+          'fix',
+          'docs',
+          'style',
+          'refactor',
+          'perf',
+          'test',
+          'build',
+          'ci',
+          'chore',
+          'revert',
+        ]],
+      },
     });
   });
 });
--- no-wip.js
+++ no-wip.js
@@ -1,4 +1,9 @@
+const { convTypes } = require('conventional-ruby-commit-types');
+
 module.exports = {
   defaultIgnores: false,
-  extends: ['@commitlint/config-conventional'],
+  extends: ['./index.js'],
+  rules: {
+    'type-enum': [2, 'always', convTypes],
+  },
 };
