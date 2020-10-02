af050f2086770da532f912cb2fe086d7b6e2e9e9
s14k51 <s14k51@gmail.com>
2020-10-02 12:44:34 -0500

    feat: update conventional types

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const { types, configureWidths } = require('conventional-ruby-commit-types');
-
-const { maxHeaderWidth, maxLineWidth } = configureWidths();
+const { types } = require('conventional-ruby-commit-types');
 
 module.exports = {
   parserPreset: 'conventional-changelog-conventionalcommits',
   rules: {
     'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'always', maxLineWidth],
+    'body-max-line-length': [2, 'always', 100],
     'footer-leading-blank': [1, 'always'],
-    'footer-max-line-length': [2, 'always', maxLineWidth],
-    'header-max-length': [2, 'always', maxHeaderWidth],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [
       2,
--- no-wip.js
+++ no-wip.js
@@ -1,9 +1,9 @@
-const { convTypes } = require('conventional-ruby-commit-types');
+const { noWIPTypes } = require('conventional-ruby-commit-types');
 
 module.exports = {
   defaultIgnores: false,
   extends: ['./index.js'],
   rules: {
-    'type-enum': [2, 'always', convTypes],
+    'type-enum': [2, 'always', noWIPTypes],
   },
 };
